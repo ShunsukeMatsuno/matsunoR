e105cc40831efcd352dc4099b21b9083d947beb9
Shunsuke Matsuno <matsuno.shunsuke@gmail.com>
2024-10-06 00:21:51 -0400

    minor

--- R/ggplot_theme_original.R
+++ R/ggplot_theme_original.R
@@ -1,25 +1,34 @@
 #' ggplot2 theme original
 #' @export
 
-theme_matsuno <- function (base_size = 11,
-                           base_family = "", 
-                           base_line_size = base_size/22,
-                           base_rect_size = base_size/22) 
-{
-  theme_grey(base_size = base_size, base_family = base_family, 
-             base_line_size = base_line_size, base_rect_size = base_rect_size) %+replace% 
-    theme(panel.background = element_rect(fill = "white", 
-                                          colour = NA), panel.border = element_rect(fill = NA, 
-                                                                                    colour = "grey20"), panel.grid = element_line(colour = "grey92"), 
-          panel.grid.minor = element_line(linewidth = rel(0.5)), 
-          strip.text.x = element_text(size = 11, color = "white", face = "bold"),
-          strip.background = element_rect(fill = "grey20"), complete = TRUE) %+replace% 
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          axis.line = element_line(colour = "black"),
-          axis.text = element_blank(),
-          axis.ticks = element_blank())
-  
+theme_matsuno <- function(base_size = 11,
+                          base_family = "",
+                          base_line_size = base_size / 22,
+                          base_rect_size = base_size / 22) {
+      theme_grey(
+            base_size = base_size, base_family = base_family,
+            base_line_size = base_line_size, base_rect_size = base_rect_size
+      ) %+replace%
+            theme(
+                  panel.background = element_rect(
+                        fill = "white",
+                        colour = NA
+                  ), panel.border = element_rect(
+                        fill = NA,
+                        colour = "grey20"
+                  ), panel.grid = element_line(colour = "grey92"),
+                  panel.grid.minor = element_line(linewidth = rel(0.5)),
+                  strip.text.x = element_text(size = 11, color = "white", face = "bold"),
+                  strip.background = element_rect(fill = "grey20"), complete = TRUE
+            ) %+replace%
+            theme(
+                  panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank(),
+                  axis.line = element_line(colour = "black"),
+                  axis.text = element_blank(),
+                  axis.ticks = element_blank(),
+                  text = element_text(family="LM Roman 10")
+            )
 }
 
 # # test
