c2c09d4abcebd2f148a57e02e231a9ab2130abf6
Shunsuke Matsuno <44533758+ShunsukeMatsuno@users.noreply.github.com>
2025-04-15 13:13:28 -0400

    minor

--- R/intervals.R
+++ R/intervals.R
@@ -20,7 +20,7 @@
 #'   \item Checks for holes between adjacent intervals
 #' }
 #' 
-#' @examples examples/intervals_example.R
+#' @example examples/intervals_example.R
 #' @export
 create_partition <- function(intervals) {
   if (!is.list(intervals) || length(intervals) == 0) {
