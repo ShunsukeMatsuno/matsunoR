7f0434b21e5221d4e55e71a5b3a5dbc64232aa1c
matsuno (ryzen) <matsuno.shunsuke@gmail.com>
2025-09-15 20:07:35 -0400

    minor

--- R/maxmin.R
+++ R/maxmin.R
@@ -1,11 +1,11 @@
-#' Censor values within the range \eqn{[0,1]}
+#' Censor values within the range \eqn{[0, 1]}
 #'
-#' This function ensures that each element in `x` is constrained within the interval \eqn{[0,1]}.
+#' This function ensures that each element in `x` is constrained within the interval \eqn{[0, 1]}.
 #' If `x` is a scalar or vector, values less than 0 are replaced with 0, and values greater than 1
 #' are replaced with 1.
 #'
 #' @param x A numeric value or numeric vector.
-#' @return A numeric value or vector with all elements censored within \eqn{[0,1]}.
+#' @return A numeric value or vector with all elements censored within \eqn{[0, 1]}.
 #' @examples
 #' maxmin(0.5) # Returns 0.5
 #' maxmin(-0.2) # Returns 0
